cd1b29380c87f6ca0d5967097a6633191e23d869
Lattimore Lau <sunnyjava@hotmail.com>
2018-12-26 17:01:01 +0800

    import projects: jlib & jmain

---
+++ jlib/src/main/java/info/ziang/App.java
@@ -0,0 +1,17 @@
+package info.ziang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static void sayHi(String name){
+        System.out.println("Hi, " + name);
+    }
+}
---
+++ jmain/src/main/java/info/ziang/AppMain.java
@@ -0,0 +1,15 @@
+package info.ziang;
+
+import info.ziang.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppMain
+{
+    public static void main( String[] args )
+    {
+        App.sayHi("world!");
+    }
+}
